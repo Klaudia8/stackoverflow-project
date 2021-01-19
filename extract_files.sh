21f491fa98a815b7efb7222d49242ca6867522e5
Klaudia8 <klaudiagorys21@gmail.com>
2021-01-19 17:09:12 +0100

    Ignore data

--- extract_files.sh
+++ extract_files.sh
@@ -16,9 +16,9 @@ while read line; do
 	s3_dir=${line::-21}
 	ls $new_dir | grep -E "(Comments|Posts|Users).xml" > temporary_file.txt
 	while read file; do
-		aws s3 mv "$new_dir/${file}" "s3://stackexchangedata/${s3_dir}/${file}" 
+		aws s3 mv "$new_dir/${file}" "s3://stackexchangedataxml/${s3_dir}/${file}" 
 	done < temporary_file.txt
-	aws s3 mv "$new_dir" "s3://stackexchangedata/${s3_dir}" --recursive 
+	aws s3 mv "$new_dir" "s3://stackexchangedataxml/${s3_dir}" --recursive 
 	# aws s3 cp --recursive "$new_dir" "s3://stackexchangedata/${s3_dir}" 
 	n=$((n+1))
 done < $filename
